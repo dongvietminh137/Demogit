90549bde45c4a7f3d03e49127cd09c6c55c760c7
dongvietminh137 <dongvietminh0705@gmail.com>
2025-03-03 23:51:09 +0700

    context

--- App.js
+++ App.js
@@ -1,62 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React from "react";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+// import SplashScreen from "./screens/SplashScreen";
+// import OnboardingScreen from "./screens/OnboardingScreen";
+// import SignInScreen from "./screens/SignInScreen";
+// import NumberScreen from "./screens/NumberScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ExplorerScreen from "./screens/ExplorerScreen";
+// import LoginScreen from "./screens/SignInScreen";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style ={ styles.login}>Đăng Nhập</Text>
-      <Text style ={ styles.dangnhap}> Nhập số điện thoại </Text>
-      <Text style ={ styles.sodienthoai}>Dùng số điện thoại để đăng nhập hoặc đăng ký tài khoản tại OneHousing Pro</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nhập số điện thoại của bạn"
-        keyboardType="phone-pad"
-      />
-     <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Tiếp tục</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="Number" component={NumberScreen} /> */}
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="ExplorerScreen" component={ExplorerScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 40,
-  },
-  login:{
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 80,
-  },
-  dangnhap:{
-    fontSize: 18,
-    marginBottom: 20,
-  },
-  sodienthoai:{
-    fontSize: 18,
-    marginBottom: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    padding: 10,
-    fontSize: 16,
-    marginBottom: 30,
-  },
-  button: {
-    backgroundColor: '#007AFF',
-    borderRadius: 8,
-    alignItems: 'center',
-    paddingVertical: 15,
-    marginTop: 240,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
+export default App;
